2f92d9d06c6601a66291685125b5a4bbcffd8eb4
Zain Fathoni <zain.fathoni@gmail.com>
2018-10-28 06:05:29 +0800

    Verify Custom JavaScript Tests with Jest

---
+++ lessons/jest.test.js
@@ -0,0 +1,16 @@
+const { sumAsync, subtractAsync } = require('../math')
+
+test('sumAsync adds numbers asynchronously', async () => {
+  const result = await sumAsync(3, 7)
+  const expected = 10
+  expect(result).toBe(expected)
+})
+
+test('subtractAsync subtracts numbers asynchronously', async () => {
+  const result = await subtractAsync(7, 3)
+  const expected = 4
+  expect(result).toBe(expected)
+})
+
+// Install Jest globally and run this command:
+// npx jest
