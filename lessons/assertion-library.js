7c53342c7af15ee09859192eff50ab364b7eaded
Zain Fathoni <zain.fathoni@gmail.com>
2018-10-27 09:52:55 +0800

    Abstract Test Assertions into a JavaScript Assertion Library - Initial

---
+++ lessons/assertion-library.js
@@ -0,0 +1,13 @@
+const { sum, substract } = require('../math')
+
+let result = sum(3, 7)
+let expected = 10
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
+
+result = substract(7, 3)
+expected = 4
+if (result !== expected) {
+  throw new Error(`${result} is not equal to ${expected}`)
+}
